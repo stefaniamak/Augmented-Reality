c49afb6283be13bfee0513aa27445617ef7398d7
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2019-04-09 16:03:28 +0300

    Full

---
+++ ΑΜ 164703 Επαυξημένη Πραγματικότητα/New Unity Project 1/Assets/InputController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnControls;
+
+public class InputController : MonoBehaviour {
+
+	// Variables for the speed and turning speed of the rabbit
+	public float speed = 5f;
+	public float turnSpeed = 5f;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector2 direction2d = JoystickDirection ();
+		Vector3 direction3d = new Vector3 (direction2d.x, 0, direction2d.y);
+
+		// Movement and Rotation
+		if (direction2d != Vector2.zero) {
+			transform.position += direction3d * Time.deltaTime * speed;
+
+			Quaternion targetRotation = Quaternion.LookRotation (direction3d);
+			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+
+			anim.SetBool ("IsWalking", true);
+		} else
+			anim.SetBool ("IsWalking", false);
+
+	}
+
+	// Get the joystick's direction from the joystick imput axis
+	Vector2 JoystickDirection (){
+		float verticalValue = CnInputManager.GetAxis ("Vertical");
+		float horizontalValue = CnInputManager.GetAxis ("Horizontal");
+
+		return new Vector2 (horizontalValue, verticalValue);
+	}		
+}
---
+++ ΑΜ 164703 Επαυξημένη Πραγματικότητα/readme.txt
@@ -0,0 +1,4 @@
+Το Android αρχείο βρήσκεται μέσα στον φάκελο New Unity Project 1 με το όνομα ProjectAndroid.apk
+Δεν ήταν δυνατή η δημειουργία PC προγράμματος αντιστιχα. 
+Αναγράφει σε σελίδες ότι δεν υποστηρίζονται τα desktop apps.
+(Πηγή: https://developer.vuforia.com/forum/device-support/can-vuforia-can-make-exe-standalone)
